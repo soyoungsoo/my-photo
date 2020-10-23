cef3bfa698ab12a3fcf877e2afb40da0dee71606
youngsoo <0303soso@gmail.com>
2020-10-23 17:43:56 +0900

    [feat] 애니메이션 추가

--- src/component/photolist/Photo.js
+++ src/component/photolist/Photo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './photo.css';
 
-function Photo({img_src}) {
-    return <article className="thumbnail">
+function Photo({img_src, index}) {
+    return <article className={`thumbnail ${index == 0 ? 'active' : ''}`}>
                 <img src={img_src}/>
             </article>;
 }
--- src/component/viewer/Viewer.js
+++ src/component/viewer/Viewer.js
@@ -3,7 +3,19 @@ import './viewer.css';
 
 function Viewer() {
     return <div id="viewer">
-                Viewer
+                <div className="inner">
+                    <div className="nav-next"></div>
+                    <div className="nav-previous"></div>
+                    <div className="toggle"></div>
+                </div>
+                <div className="slider">
+                    <div className="slide-image">
+                        <div className="caption">
+                            <h2>Mattis lorem sodales</h2>
+                            <p>Feugiat auctor leo massa, nec vestibulum nisl eratfaucibus, rutrum nulla.</p>
+                        </div>
+                    </div>
+                </div>
             </div>;
 }
 
