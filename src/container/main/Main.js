7118aa155fa537442585c1a57cceb3f16da2caf2
youngsoo <0303soso@gmail.com>
2020-10-27 17:13:38 +0900

    [feat] axios 추가

--- src/container/main/Main.js
+++ src/container/main/Main.js
@@ -1,14 +1,19 @@
 import React from 'react';
-
 import Header from 'component/header/Header';
 import PhotoList from 'container/photolist/PhotoList';
 import Footer from 'component/footer/Footer';
 import Viewer from 'component/viewer/Viewer';
+import './main.css';
+import 'assets/css/keyframs.css';
+import API from '../../axios';
 
 function Main() {
+    const response = API.get('/api/v1/photo/json', {});
+    console.log(response);
+
     return <div id="Main" style={mainStyle}>
                 <Viewer/>
-                <div id="info" style={infoStyle}>
+                <div id="photoInfo" style={infoStyle}>
                     <Header/>
                     <PhotoList/>
                     <Footer/>
